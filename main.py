9e8e4b86f5618ef7a1fde28d310170f5377db915
samwelkanda <samwelkanda@gmail.com>
2021-01-29 15:48:07 +0300

    Add blog endpoints to main file

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from typing import Optional
+from typing import List, Optional
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
@@ -11,4 +26,30 @@ def read_root():
 @app.get("/items/{item_id}")
 def get_item(item_id: int, q: Optional[str] = None):
     return{ "item_id": item_id, "q": q}
-    
+
+@app.get("/posts", response_model = List[schemas.Post])
+def get_posts(skip: int = 0, limit: int = 50, db: Session = Depends(get_db)):
+    return crud.get_posts(db=db, skip=skip, limit=limit)
+
+@app.post("/posts", response_model = schemas.Post, status_code=201):
+def create_post(post: schemas.CreatePost, db: Session = Depends(get_db)):
+    return crud.create_post(db=db, post=post)
+
+@app.get("/posts/{post_id}", response_model = schemas.Post)
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    item = crud.get_post(db=db, id=post_id)
+    if item is None:
+        raise HTTPException(status_code=404, detail="Post not found.")
+    return item
+
+@app.put("/posts/{post_id}")
+async def replace_post(post_id: int, post: schemas.CreatePost, db: Session = Depends(get_db)):
+    return crud.update_post(db=db, post_id=post_id, post=post)
+
+@app.patch("/posts/{post_id}")
+async def update_post(post_id: int, post: schemas.CreatePost, db: Session = Depends(get_db)):
+    return crud.update_post(db=db, post_id=post_id, post=post)
+
+@app.delete("/posts/{post_id}", status_code=204)
+async def delete_post(post_id: int, db: Session = Depends(get_db)):
+    return crud.delete_post(db=db, post_id=post_id)
