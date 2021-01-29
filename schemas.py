34c8f4d25645c0694763f30fabcabc51bbac9cc1
samwelkanda <samwelkanda@gmail.com>
2021-01-29 16:38:21 +0300

    Add schema for post

---
+++ schemas.py
@@ -0,0 +1,15 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+class PostBase(BaseModel):
+    title: str
+    body: str
+
+class PostCreate(PostBase):
+    pass
+
+class Post(PostBase):
+    id: int
+
+    class Config:
+        orm_mode = True
