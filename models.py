e4158e6bf18ce82b3ae18e9e886995d3e7c5924e
samwelkanda <samwelkanda@gmail.com>
2021-01-29 16:32:47 +0300

    Add database model

---
+++ models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
